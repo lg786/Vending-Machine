3986fdd20ce8e5f82a3b9fe5b154a987106c19e1
Khooshrin Aspi Pithawalla <88196065+Khooshrin@users.noreply.github.com>
2022-04-11 16:24:53 +0530

    Add files via upload

--- VendingMachine.cpp
+++ VendingMachine.cpp
@@ -26,50 +26,64 @@ int main()
     switch (choice)
     {
         case 1:
-            cout<<"Description\tID\tQuantity\tPrice\n";
-            for(int i=0;i<9;i++)
-            {
-                cout<<items[i].first<<"\t\t"<<items[i].second.first<<"\t"<<items[i].second.second.first<<"\t\t"<<items[i].second.second.second<<endl;
-            }
-            cout<<"\n\nEnter Item ID and Quantity for each item you want to purchase.\nWhen done please enter -1.\n\n";
-            int itemCode=0,quantity=0;
-            map<int,int> order;
             while(true)
             {
-                cin >> itemCode;
-                if(itemCode==-1)
-                    break;
-                else if(order.find(itemCode)!=order.end())
+                cout<<"Description\tID\tQuantity\tPrice\n";
+                for(int i=0;i<9;i++)
                 {
-                    cout<<"This item has already been purchased.\n";
+                    cout<<items[i].first<<"\t\t"<<items[i].second.first<<"\t"<<items[i].second.second.first<<"\t\t"<<items[i].second.second.second<<endl;
                 }
-                else if(itemCode>=1 && itemCode<=9)
+                cout<<"\n\nEnter Item ID and Quantity for each item you want to purchase.\nWhen done please enter -1.\n\n";
+                int itemCode=0,quantity=0;
+                map<int,int> order;
+                while(true)
                 {
-                    cin >> quantity;
-                    if(quantity>items[itemCode-1].second.second.first)
+                    cin >> itemCode;
+                    if(itemCode==-1)
+                        break;
+                    else if(order.find(itemCode)!=order.end())
                     {
-                        cout << "Quantity you wish to purchase exceeds quantity in Vending Machine.\n Quantity Available: "<<items[itemCode-1].second.second.first<<endl;
-                        cout << "If you wish to change your amount please re-enter the quantity else if you do not want to buy this item enter -1\n";
+                        cout<<"This item has already been purchased.\n";
+                    }
+                    else if(itemCode>=1 && itemCode<=9)
+                    {
                         cin >> quantity;
-                        if(quantity==-1)
-                            continue;
+                        if(quantity>items[itemCode-1].second.second.first)
+                        {
+                            cout << "Quantity you wish to purchase exceeds quantity in Vending Machine.\n Quantity Available: "<<items[itemCode-1].second.second.first<<endl;
+                            cout << "If you wish to change your amount please re-enter the quantity else if you do not want to buy this item enter -1\n";
+                            cin >> quantity;
+                            if(quantity==-1)
+                                continue;
+                            else
+                                order.insert(pair<int,int>(itemCode,quantity));
+                        }
                         else
                             order.insert(pair<int,int>(itemCode,quantity));
                     }
                     else
-                        order.insert(pair<int,int>(itemCode,quantity));
+                        cout << "Invalid Item ID\n";
+                }
+                if(order.empty()==false)
+                {
+                    cout << "Your order is below.\nItem ID \tQuantity\n";
+                    map<int, int>::iterator itr;
+                    for(itr=order.begin();itr!=order.end();++itr)
+                        cout << itr->first << "\t\t" << itr->second << endl;
+                    int confirm=-2;
+                    cout << "Please confirm your order. Enter 1 to confirm, 0 to redo the order or -1 to exit." << endl;
+                    cin >> confirm;
+                    if (confirm==1)
+                        break;
+                    else if(confirm==-1)
+                        return 0;
+                    else if(confirm==0)
+                        continue;
                 }
                 else
-                    cout << "Invalid Item ID\n";
-            }
-            if(order.empty()==false)
-            {
-                cout << "Item ID \tQuantity\n";
-                map<int, int>::iterator itr;
-                for(itr=order.begin();itr!=order.end();++itr)
-                    cout << itr->first << "\t\t" << itr->second << endl;
+                    break;
             }
     }
 
-return 0;
+    return 0;
 }
