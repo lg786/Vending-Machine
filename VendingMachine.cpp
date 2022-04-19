aa26b16b22360834b8e403552889c0eb5227b024
lg786 <70192357+lg786@users.noreply.github.com>
2022-04-19 13:02:55 +0530

    Add files via upload

--- VendingMachine.cpp
+++ VendingMachine.cpp
@@ -51,16 +51,14 @@ int main()
                                 cout<<"This item has already been purchased.\n";
                             else if(itemCode>=1 && itemCode<=9)
                             {
-                                if(quantity>items[itemCode-1].second.second.first)
+                                while(quantity>items[itemCode-1].second.second.first)
                                 {
                                     cout << "Quantity you wish to purchase exceeds quantity in Vending Machine.\nQuantity Available: "<<items[itemCode-1].second.second.first<<endl;
                                     cout << "If you wish to change your amount please re-enter the quantity else if you do not want to buy this item enter -1\n";
                                     cin >> quantity;
-                                    if(quantity==-1)
-                                        continue;
-                                    else
-                                        order.insert(pair<int,int>(itemCode,quantity));
                                 }
+                                if(quantity==-1)
+                                    continue;                                
                                 else
                                     order.insert(pair<int,int>(itemCode,quantity));
                             }
@@ -91,7 +89,7 @@ int main()
                             cost=cost+itr->second*items[itr->first-1].second.second.second;
                         }
                         cout << "\nFinal Price to Pay: " << cost << endl;
-                        cout << "\nThe machine accepts only certain denominations.\nPlease enter number of notes of each denomination you wish to pay with.";
+                        cout << "\nThe machine accepts only certain denominations.\nPlease enter number of notes of each denomination you wish to pay with.\n";
                         int amt=0;
                         map<int,int> pay;
                         while(true)
@@ -109,7 +107,8 @@ int main()
                             }
                             if(amt<cost)
                             {
-                                cout << "Full amount not paid\n"; 
+                                cout << "Full amount not paid.\nPayment Amount Remaining:" << (cost-amt) << "\nPlease take your money back and re-enter your payment amount.\n"; 
+                                pay.clear();
                                 continue;
                             }
                             else
@@ -144,7 +143,7 @@ int main()
                             }
                             if(chng==0)
                             {
-                                cout << "\nPlease accept your change.\nDenomination\tNumber of Notes\n";
+                                cout << "\nPayment Accepted.\nPlease accept your change.\nDenomination\tNumber of Notes\n";
                                 for(itr=change.begin();itr!=change.end();++itr)
                                 {
                                     cout << itr->first << "\t\t" << itr->second << endl;
@@ -154,20 +153,23 @@ int main()
                             }
                             else
                             {
-                                cout << "\nNot enough change in the machine. Please take your money back.";
-                                cout << "Change remaining: " << chng;
+                                cout << "\nNot enough change in the machine.\nPlease take your money back.\nYour order has been cancelled.";
                                 for(itr=pay.begin();itr!=pay.end();++itr)
                                 {
                                     map<int,int> :: iterator it = money.find(itr->first);
                                     it->second=it->second-itr->second;
+                                }
+                                for(itr=order.begin();itr!=order.end();++itr)
+                                {
+                                    items[itr->first-1].second.second.first=items[itr->first-1].second.second.first+itr->second;
                                 }
                                 break;
                             }
                         }
                         else
                         {
-                            cout << "Payment Accepted.\nThank you for using the Vending Machine.\n";
-                            return 0;
+                            cout << "Payment Accepted.\nNo change has to be returned.\nThank you for using the Vending Machine.\n";
+                    
                         }
                     }
                     else
@@ -177,13 +179,87 @@ int main()
                 break;
             case 2:
             {
-                cout<<"Description\tID\tQuantity\tPrice\n";
-                for(int i=0;i<9;i++)
+                map<int,int> restock;
+                while (true)
                 {
-                    cout<<items[i].first<<"\t\t"<<items[i].second.first<<"\t"<<items[i].second.second.first<<"\t\t"<<items[i].second.second.second<<endl;
+                    cout << "Displaying the current stocks of items :\n";
+                    for(int i=0;i<9;i++)
+                    {
+                        cout<<items[i].first<<"\t\t"<<items[i].second.first<<"\t"<<items[i].second.second.first<<"\t\t"<<items[i].second.second.second<<endl;
+                    }
+                    cout<<"Enter item ID of item you want to restock and quantity by which you want to restock\nWhen done please enter -1.\n\n";
+                    int itemCode=0,restockQuantity=0;
+                        while(true)
+                        {
+                            cin >> itemCode;
+                            if(itemCode==-1)
+                                break;
+                            else
+                            { 
+                                cin >> restockQuantity;
+                               if(itemCode>=1 && itemCode<=9)
+                                {
+                                    if((restockQuantity+items[itemCode-1].second.second.first)>10)
+                                    {
+                                        cout << "Quantity you wish to restock exceeds the maximum capacity of the item chosen\n The maz capacity for the chosen item is 10. Current quantity: "<<items[itemCode-1].second.second.first<<endl;
+                                        cout << "If you wish to change the quantity please re-enter the quantity or else enter -1\n";
+                                        cin >> restockQuantity;
+                                        if(restockQuantity==-1)
+                                               continue;
+                                        else
+                                            items[itemCode-1].second.second.first = items[itemCode-1].second.second.first + restockQuantity;
+                                            restock.insert(pair<int,int>(itemCode,restockQuantity));
+                                         
+                                    }
+                                     else
+                                        items[itemCode-1].second.second.first = items[itemCode-1].second.second.first + restockQuantity;
+                                        restock.insert(pair<int,int>(itemCode,restockQuantity));
+                                        
+                                
+                                }
+                                else
+                                cout << "Invalid Item ID\n";
+                            }
+                        }
+                
+                    if(restock.empty()==false)
+                    {
+                        cout << "\nYour restock request is below.\nDescription\tItem ID \tQuantity\n";
+                         map<int, int>::iterator itr;
+                         for(itr=restock.begin();itr!=restock.end();++itr)
+                        cout << items[itr->first-1].first << "\t\t" << itr->first << "\t\t" << itr->second << endl;
+                        int confirm=-2;
+                         cout << "Please confirm your order. Enter 1 to confirm, 0 to redo the order or -1 to exit." << endl;
+                        cin  >> confirm;
+                        
+                        if(confirm==-1)
+                            return 0;
+                        else if(confirm==0)
+                        {
+                        restock.clear();
+                            items[itemCode-1].second.second.first = items[itemCode-1].second.second.first - restockQuantity;
+                            continue;
+                        }
+                        
+                        cout << "\nThe updated stock is below.\nDescription\tItem ID \tQuantity\n";
+                        
+                        
+                        for(int i=0;i<9;i++)
+                        {
+                            cout<<items[i].first<<"\t\t"<<items[i].second.first<<"\t"<<items[i].second.second.first<<"\t\t"<<items[i].second.second.second<<endl;
+                        }
+                       break;
+                       cout << "Thank you for using the Vending Machine.\n";
+
+                    }
+                    else
+                    break;
+
                 }
+                
+                
             }
-                break;
+            break;
             case 3: 
             {
                 cout << "Denominations\tNumber of Notes\n";
